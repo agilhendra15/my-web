04e19fcddef3b5feea172a8b846fcce68e948215
agilhendra15 <112374151+agilhendra15@users.noreply.github.com>
2022-08-29 13:26:30 +0700

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+    </script>
+    <header>
+        <?php include 'nav.html'; ?>
+    </header>
+
+    <div class="card" style="width: 18rem;">
+        <img src="agill.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Agil Hendra Wardhana</h5>
+            <p class="card-text">Kelas: XII RPL 1</p>
+            <p class="card-text">No: 01</p>
+
+            <a href="https://www.instagram.com/agil_hw25/" class="btn btn-primary">instagram</a>
+        </div>
+    </div>
+
+</body>
+
+</html>
